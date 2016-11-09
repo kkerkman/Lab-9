3fda7b7d29a67ad13d880d9c3ee302dd58e7a835
Katie Kerkman <kkerkman@yahoo.com>
2016-11-09 16:35:30 -0500

    firs lab 9 commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$("li").mouseenter(function(){
+	$(this).fadeTo("fast", 0.25);
+	});
+$("li").mouseleave(function(){
+	$(this).fadeTo("fast", 1);
+	});
+$("li").click(function(){
+	$(this).toggleClass(function(){
+		$(this).css('background-color', 'LightBlue');
+		});
+	});
+$("li").click(function(){
+	$(this).text("Clicked");
+});
+
+});
